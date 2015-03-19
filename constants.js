ebfa3dc2b9a1c5614be171ebc2db7333422a27fc
umelec <info@fclerc.ch>
2015-03-19 10:26:38 +0100

    camera

--- constants.js
+++ constants.js
@@ -2,4 +2,6 @@ angular.module("citizen-engagement.constants", [])
 .constant("apiUrl", "@apiUrl@")
 .constant("mapboxMapId", "@mapboxMapId@")
 .constant("mapboxAccessToken", "@mapboxAccessToken@")
+.constant("qimgUrl", "@qimgUrl@")
+.constant("qimgToken", "@qimgToken@")
 ;
